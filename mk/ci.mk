b18ebb4f07f8e99133652866c45cc4442c0439d6
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2017-01-23 15:30:19 +0100

    ci.mk: initial checkin
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

---
+++ mk/ci.mk
@@ -0,0 +1,37 @@
+ci-deploy ci-deploy-rescue ci-deploy-sdk ci-deploy-doc:
+	: D=$(abspath $D)
+	: PWD=$(abspath .)
+	rm -rf $D
+	mkdir -p $D
+	$(MAKE) .$@ BUILDMODE=ci BUILDVARS='${BUILDVARS}'
+	ls -l "$D"/
+
+ci-build:
+	$(MAKE) .$@ BUILDMODE=ci BUILDDIR="../build${CI_FLAVOR}-${CI_DIST}" C="build${CI_FLAVOR}" top_srcdir="$(abspath .)"
+
+ifeq (${BUILDMODE},ci)
+override BUILDMODE = .ci
+T ?= image
+
+.ci-deploy:
+	ln $S/images/*/* $D/
+
+.ci-deploy-sdk:
+	ln $S/sdk/* $D/
+
+.ci-build:	.ci-prepare
+	$(MAKE) $T
+	touch ${BUILDDIR}/conf/.ok
+	${CI_DIR}/source-distribute /cache/sources
+	$(MAKE) "ci-deploy${CI_FLAVOR}" D="_deploy${CI_FLAVOR}" S=${BUILDDIR}/deploy
+
+.ci-prepare:	tmpl.conf
+ifneq ($(CI_FLAVOR),)
+	ln -s build "build${CI_FLAVOR}"
+endif
+	${CI_DIR}/oe-conf "$C" "${BUILDDIR}" "$(abspath $<)" "local-local.conf" 0
+ifneq (${CI_MACHINE},)
+	sed -i -e "1i\\" -e "MACHINE = \"${CI_MACHINE}\"" ${BUILDDIR}/conf/local.conf
+endif
+
+endif				# BUILDMODE == ci
