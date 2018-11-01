925fb9c949850041ebed70fb11715b6df5c9dbf8
Enrico Scholz <enrico.scholz@ensc.de>
2018-11-01 10:43:34 +0100

    buildvars: added selinux-policy mk
    Signed-off-by: Enrico Scholz <enrico.scholz@ensc.de>

---
+++ scripts/buildvars/selinux-policy.mk
@@ -0,0 +1,28 @@
+include ${BUILDVAR_BUILDVARS_SCRIPT_DIR}/_generic_.mk
+
+MODULE_PRIO ?= 800
+POLICY_FILE ?= policy.31
+POLICY_FILE_SRC = ${BUILDVAR_SE_POLICY_ROOT}/etc/selinux/${BUILDVAR_SE_POLICY_NAME}/policy/${POLICY_FILE}
+
+SEMODULE = semodule \
+  -p '${BUILDVAR_SE_POLICY_ROOT}' \
+  -s '${BUILDVAR_SE_POLICY_NAME}' \
+  -n
+
+modules:
+	${MAKE} ${BUILDVAR_SE_OEMAKE} all
+
+policy:	modules
+	${MAKE} -f ${firstword ${MAKEFILE_LIST}} .stamp-policy
+
+.stamp-policy:	${POLICY_FILE}
+	@touch $@
+
+${POLICY_FILE}:	${POLICY_FILE_SRC}
+	install -p -m 0644 $< $@
+
+${POLICY_FILE_SRC}:	$(wildcard *.pp)
+	${SEMODULE} -X ${MODULE_PRIO} -i *.pp
+	rm -rf ${BUILDVAR_SE_POLICY_ROOT}/var/lib/selinux/${BUILDVAR_SE_POLICY_NAME}/active/modules/${MODULE_PRIO}
+
+.PHONY:	modules policy
