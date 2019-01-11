7372cfef4b0637f0bdb31414f7e30b801b0c174e
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2019-01-11 22:45:36 +0100

    ci.mk: backported
    from 14968f43abc82d8775e290a965f8b067f4e38a73
    
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

--- mk/ci.mk
+++ mk/ci.mk
@@ -4,26 +4,48 @@ ci-deploy ci-deploy-rescue ci-deploy-sdk ci-deploy-doc:
 	rm -rf $D
 	mkdir -p $D
 	$(MAKE) .$@ BUILDMODE=ci BUILDVARS='${BUILDVARS}'
-	ls -l "$D"/
+	ls -lR "$D"/
 
 ci-build:
 	$(MAKE) .$@ BUILDMODE=ci BUILDDIR="../build${CI_FLAVOR}-${CI_DIST}" C="build${CI_FLAVOR}" top_srcdir="$(abspath .)"
 
 ifeq (${BUILDMODE},ci)
 override BUILDMODE = .ci
+
+ifeq (${CI_MULTI_MACHINE},)
 T ?= image
+CI_DEPLOY_MACHINES ?=
+else
+T ?= all-images
+CI_DEPLOY_MACHINES ?= ${MACHINES}
+endif
 
-.ci-deploy:
+.ci-deploy .ci-deploy-sdk .ci-build:.%:				%-pre ..% %-post
+ci-deploy-pre ci-deploy-sdk-pre ci-build-pre:%-pre:
+ci-deploy-post ci-deploy-sdk-post ci-build-post:%-post:		..%
+..ci-deploy ..ci-deploy-sdk ..ci-build:..%:			%-pre
+
+ifeq (${CI_DEPLOY_MACHINES},)
+..ci-deploy:
 	ln $S/images/*/* $D/
+else
+..ci-deploy:	$(addprefix ci-deploy_machine-,${CI_DEPLOY_MACHINES})
+endif
 
-.ci-deploy-sdk:
+$(addprefix ci-deploy_machine-,${CI_DEPLOY_MACHINES}):	ci-deploy-pre
+
+$(addprefix ci-deploy_machine-,${CI_DEPLOY_MACHINES}):ci-deploy_machine-%:
+	install -d -m 0755 '$D/$*'
+	ln $S/images/'$*'/* '$D/$*/'
+
+..ci-deploy-sdk:
 	ln $S/sdk/* $D/
 
-.ci-build:	.ci-prepare
+..ci-build:		.ci-prepare
 	$(MAKE) $T
 	touch ${BUILDDIR}/conf/.ok
 	${CI_DIR}/source-distribute /cache/sources
-	$(MAKE) "ci-deploy${CI_FLAVOR}" D="_deploy${CI_FLAVOR}" S=${BUILDDIR}/deploy
+	$(MAKE) "ci-deploy${CI_FLAVOR}" D="_deploy${CI_FLAVOR}" S=${DEPLOY_DIR}
 
 .ci-prepare:	tmpl.conf
 ifneq ($(CI_FLAVOR),)
