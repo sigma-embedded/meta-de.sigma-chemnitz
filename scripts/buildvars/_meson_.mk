ebec197482cd666330cc6196a6f20d6a4135df82
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2020-01-27 23:19:37 +0100

    buildvars: added _meson_.mk
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
    (cherry picked from commit 97b4f90afb0fdc9019428019aae420b3452e1844)

---
+++ scripts/buildvars/_meson_.mk
@@ -0,0 +1,30 @@
+include ${BUILDVAR_BUILDVARS_SCRIPT_DIR}/_generic_.mk
+
+.DEFAULT_GOAL = meson-install
+
+MESON_ARGS = \
+	${BUILDVAR_MESONOPTS} \
+	${BUILDVAR_MESON_CROSS_FILE} \
+
+
+MESON ?= meson
+NINJA ?= ninja
+SED ?=   sed
+
+MESON_CDB_CLEANUP = \
+	-e "s!-MF \+'[^'']*'!!g"
+
+meson-setup:	export PKG_CONFIG_LIBDIR=${BUILDVAR_PKG_CONFIG_LIBDIR}
+meson-setup:
+	${MESON} setup --wipe ${MESON_ARGS} '.' '$S'
+
+meson-build:
+	${NINJA}
+
+meson-install:
+	${MESON} install
+
+meson-cdb:
+	@test -d '$S' || { echo 'source directory $$S not set or missing' >&2; exit 1; }
+	@rm -f $S/compile_commands.json
+	${SED} ${MESON_CDB_CLEANUP} < compile_commands.json > $S/compile_commands.json
