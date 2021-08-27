2de6933d67a45ce5278dbe5f5703e50cf340e4b5
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2021-08-27 23:19:29 +0200

    cargo.mk: initial checkin
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

---
+++ scripts/buildvars/cargo.mk
@@ -0,0 +1,39 @@
+include ${BUILDVAR_BUILDVARS_SCRIPT_DIR}/_generic_.mk
+
+_vars = \
+	CARGO \
+	CARGO_HOME \
+	CARGO_TARGET_SUBDIR \
+	CC \
+	CXX \
+	TARGET_CC \
+	TARGET_CXX \
+	HOST_CC \
+	HOST_CXX \
+	RUST_TARGET_PATH \
+	RUSTFLAGS \
+	PKG_CONFIG_DIR \
+	PKG_CONFIG_LIBDIR \
+	PKG_CONFIG_PATH \
+	PKG_CONFIG_SYSROOT_DIR \
+
+$(call export_vars,${_vars},build build-release install install-release)
+
+export CARGO_TARGET_DIR = ${BUILDVAR_B}/local-build
+export PKG_CONFIG_ALLOW_CROSS = 1
+
+CARGO_BUILD_FLAGS = --target ${BUILDVAR_HOST_SYS}
+
+all:	build
+
+build:
+	${CARGO} build ${CARGO_BUILD_FLAGS}
+
+build-release:
+	${CARGO} build --release ${CARGO_BUILD_FLAGS}
+
+install:
+	${CARGO} install ${CARGO_BUILD_FLAGS} --path '.' --root '${DESTDIR}/usr' --debug --force
+
+install-release:
+	${CARGO} install ${CARGO_BUILD_FLAGS} --path '.' --root '${DESTDIR}/usr' --force
