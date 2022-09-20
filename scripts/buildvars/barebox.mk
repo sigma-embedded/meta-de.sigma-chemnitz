9173a086dbbc54644fc81295902f4ca7d6fbd8d5
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2022-09-20 17:34:51 +0200

    barebox: fix 'menuconfig' target
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

--- scripts/buildvars/barebox.mk
+++ scripts/buildvars/barebox.mk
@@ -6,10 +6,14 @@ BAREBOX_ARGS= \
 	LD="${BUILDVAR_KERNEL_LD}" \
 	AR="${BUILDVAR_KERNEL_AR}" \
 	CROSS_COMPILE="${BUILDVAR_TARGET_PREFIX}" \
-	PKG_CONFIG_PATH='${BUILDVAR_STAGING_DIR_NATIVE}/usr/lib/pkgconfig' \
+	PKG_CONFIG_LIBDIR='${BUILDVAR_STAGING_DIR_NATIVE}/usr/lib/pkgconfig' \
 
 %:
 	${MAKE} ${BAREBOX_ARGS} $@
 
 .generic-build:
 	${MAKE} ${BAREBOX_ARGS}
+
+## required for building 'menuconfig' with correct libncurses
+menuconfig nconfig gconfig:	export PKG_CONFIG_LIBDIR=${BUILDVAR_STAGING_DIR_NATIVE}/usr/lib/pkgconfig
+menuconfig nconfig gconfig:	export LD_LIBRARY_PATH=${BUILDVAR_STAGING_DIR_NATIVE}/usr/lib
