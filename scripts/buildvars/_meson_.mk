674caff189ad20f37e774b089b6dfc7f7c129fd8
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2024-06-20 17:06:15 +0200

    buildvars:meson: support more meson commands
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

--- scripts/buildvars/_meson_.mk
+++ scripts/buildvars/_meson_.mk
@@ -28,3 +28,29 @@ meson-cdb:
 	@test -d '$S' || { echo 'source directory $$S not set or missing' >&2; exit 1; }
 	@rm -f $S/compile_commands.json
 	${SED} ${MESON_CDB_CLEANUP} < compile_commands.json > $S/compile_commands.json
+
+meson-test:
+	${MESON} test ${MESON_TEST_ARGS}
+
+meson-coverage:
+	${NINJA} coverage ${MESON_COVERAGE_ARGS}
+
+meson-introspect:
+	${MESON} introspect ${MESON_INTROSPECT_ARGS}
+
+meson-devenv:
+	${MESON} devenv ${MESON_DEVENV_ARGS}
+
+meson-wrap:
+	${MESON} wrap ${MESON_WRAP_ARGS}
+
+meson-help:
+	@echo "  meson-setup      [MESON_ARGS=...]"
+	@echo "  meson-build"
+	@echo "  meson-install"
+	@echo "  meson-cdb                                ... creates compile database"
+	@echo "  meson-test       [MESON_TEST_ARGS=...]"
+	@echo "  meson-coverage   [MESON_COVERAGE_ARGS=...]"
+	@echo "  meson-introspect [MESON_INTROSPECT_ARGS=...]"
+	@echo "  meson-devenv     [MESON_DEVENV_ARGS=...]"
+	@echo "  meson-wrap       [MESON_WRAP_ARGS=...]"
