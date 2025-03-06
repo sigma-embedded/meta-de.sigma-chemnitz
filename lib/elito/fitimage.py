d998a1ae33b67b3d8a6ea2ca3939cca48fa85199
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2025-03-06 12:52:50 +0100

    fitimage: replace global variables with FitSignature object
    This makes it possible to create multiple signture nodes and avoids
    races when two recipes specifiy different signing params
    
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
    (cherry picked from commit 421234cc2a41b2b4749dad9e31ec8a5d18d512fc)

--- lib/elito/fitimage.py
+++ lib/elito/fitimage.py
@@ -2,22 +2,27 @@ from abc import ABC, abstractmethod
 from functools import cmp_to_key
 import os.path
 
-_hash_algo = 'sha256'
-_sign_algo = 'sha256,rsa2048'
-_sign_key  = None
+class FitSignature:
+    def __init__(self, hash_algo = "sha256", ):
+        self.hash_algo = hash_algo
+        self.signatures = []
 
-def add_hash_sign(node):
-    global _hash_algo
-    global _sign_algo
-    global _sign_key
+    def add_key(self, hint, sign_algo = "sha256,rsa2048"):
+        self.signatures.append((hint, sign_algo))
 
-    node.add_node(OfNode("hash-1", None)
-                  .add_prop_string("algo", _hash_algo))
+    def add_hash(self, node):
+        node.add_node(OfNode("hash-1", None)
+                      .add_prop_string("algo", self.hash_algo))
 
-    if _sign_key:
-        node.add_node(OfNode("signature-1", None)
-                      .add_prop_string("algo", _sign_algo)
-                      .add_prop_string('key-name-hint', _sign_key))
+    def add_hash_sign(self, node):
+        self.add_hash(node)
+
+        idx = 1
+        for (hint, algo) in self.signatures:
+            node.add_node(OfNode("signature-%d" % idx, None)
+                          .add_prop_string("algo", algo)
+                          .add_prop_string('key-name-hint', hint))
+            idx += 1
 
 class OfValue:
     def __init__(self, val):
@@ -265,19 +270,6 @@ class FitImage(OfNode):
         return super().finish()
 
     @staticmethod
-    def set_global_hash_algo(algo):
-        global _hash_algo
-        _hash_algo = algo
-
-    def set_global_sign_algo(algo):
-        global _sign_algo
-        _sign_algo = algo
-
-    def set_global_sign_key(key):
-        global _sign_key
-        _sign_key = key
-
-    @staticmethod
     def get_hwid(dtb, id_attr, desc_attr):
         import subprocess
         with subprocess.Popen(["fdtget", dtb, "/", id_attr], stdout = subprocess.PIPE) as proc:
@@ -293,7 +285,8 @@ class FitImage(OfNode):
     @staticmethod
     def from_desc(kernels = [], dtbs = [], overlays = [], ramdisks = [],
                   id_attr = "device-id", desc_attr = "device-description",
-                  cfgnode = None, cfg_ramdisks = [], cfg_overlays = []):
+                  cfgnode = None, cfg_ramdisks = [], cfg_overlays = [],
+                  signature = FitSignature()):
         images = OfNode("&images")
 
         if len(dtbs) != 1 and cfgnode and cfg_overlays:
@@ -310,7 +303,7 @@ class FitImage(OfNode):
 
         for d in dtbs + overlays:
             (hw_id, hw_desc) = FitImage.get_hwid(d, id_attr, desc_attr) or (0, d)
-            part = (FitPart("fdt", hw_id)
+            part = (FitPart("fdt", signature, hw_id)
                     .set_type("flat_dt")
                     .set_compression("none")
                     .set_description(hw_desc)
@@ -320,7 +313,7 @@ class FitImage(OfNode):
 
         idx = 0
         for r in ramdisks:
-            part = (FitPart("ramdisk", idx)
+            part = (FitPart("ramdisk", signature, idx)
                     .set_os("linux")
                     .set_compression("none")
                     .set_inputfile(r))
@@ -344,14 +337,14 @@ class FitImage(OfNode):
                     p.push(OfValueNodeName(dtb_map[ov]))
                 cfg.add_prop(OfProperty('fdt', p))
 
-            add_hash_sign(cfg)
+            signature.add_hash_sign(cfg)
 
         cfg_root = OfNode("/")
         cfg_conf = OfNode("configurations")
 
         for d in overlays:
             part = dtb_map[d]
-            cfg_conf.add_node(FitOverlayConfNode('overlay', part))
+            cfg_conf.add_node(FitOverlayConfNode('overlay', signature, part))
 
         cfg_root.add_node(cfg_conf)
 
@@ -362,9 +355,10 @@ class FitNode(OfNode):
         super().__init__(name, instance, True)
 
 class FitOverlayConfNode(FitNode):
-    def __init__(self, prefix, part):
+    def __init__(self, prefix, signature, part):
         super().__init__(prefix, part.instance)
         self.part = part
+        self.signature = signature
 
     def finish(self):
         part = self.part
@@ -372,14 +366,12 @@ class FitOverlayConfNode(FitNode):
         self.add_prop(OfProperty('fdt', OfValueNodeName(part)))
         self.add_prop_string('description', part.desc);
 
-        add_hash_sign(self)
+        self.signature.add_hash_sign(self)
 
         return super().finish()
 
 class FitPart(FitNode):
-    def __init__(self, type, instance = None):
-        global _hash_algo
-
+    def __init__(self, type, signature, instance = None):
         super().__init__(type, instance)
 
         self.type = type
@@ -388,7 +380,7 @@ class FitPart(FitNode):
         self.desc = None
         self.arch = "${ARCH}"
         self.compression = None
-        self.hash = _hash_algo
+        self.signature = signature
         self.entry = None
         self.load = None
         self.os = None
@@ -444,8 +436,7 @@ class FitPart(FitNode):
             (self.entry, "entry",             OfPropertyH32),
         ])
 
-        self.add_node(OfNode("hash-1", None)
-                      .add_prop_string("algo", self.hash))
+        self.signature.add_hash(self)
 
         return super().finish()
 
@@ -462,7 +453,7 @@ if __name__ == '__main__':
     image = (OfTree()
              .add_node(FitImage()
                        .add_node(OfNode("images")
-                                 .add_node(FitPart("kernel")
+                                 .add_node(FitPart("kernel", FitSignature)
                                            .set_inputfile("vmlinuz.gz"))))
              .finish())
 
